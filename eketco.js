f0ab558b04a04f18fab1eb04fbc41dd866558ae5
Duda-Silva2008 <00001097324655sp@al.educacao.sp.gov.br>
2024-11-21 08:54:28 -0300

    Create eketco.js

---
+++ eketco.js
@@ -0,0 +1,28 @@
+ // variaveis da bolinha
+ let xbolinha = 300;
+ let ybolinha = 200;
+ let diametro = 20;
+ let raio = diametro/2;
+
+
+let velocidadex = 6;
+let velocidadey = 4;
+
+// variaveis da raquete
+ let larguraraquete = 10;
+ let alturaraquete = 60;
+
+let xraquete = 0;
+let yraquete = 170;
+
+let xraqueteoponente = 590;
+let yraqueteoponente = 170;
+
+let meuspontos = 0;
+let pontosoponente = 0;
+
+let ponto;
+let raquetada;
+let trilha;
+
+function s
